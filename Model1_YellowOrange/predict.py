d4a5abc0817cf3ad36d4037c316356d12c11e2a5
黄成梓 <126266928+yelloworange10@users.noreply.github.com>
2023-12-20 08:46:07 +0800

    Update predict.py

--- Model1_YellowOrange/predict.py
+++ Model1_YellowOrange/predict.py
@@ -1,22 +1,55 @@
 import torch
-from models import ImageCaptioningModel
-from datasets import FashionDataset
-from configurations import Config
+from PIL import Image
+from torchvision import transforms
+from models import AttentionModel  
+from configurations import Config  
+import json
 
-def predict(image_path, model_path):
-    # 加载配置和模型
-    config = Config()
-    model = ImageCaptioningModel(config.embed_size, config.vocab_size, config.num_layers, config.num_heads, config.dropout)
+def load_model(model_path, vocab, config):
+    model = AttentionModel(
+        image_code_dim=config.image_code_dim,
+        vocab=vocab,  # 传递词汇表字典
+        word_dim=config.word_dim,
+        attention_dim=config.attention_dim,
+        hidden_size=config.hidden_size,
+        num_layers=config.num_layers
+    )
     model.load_state_dict(torch.load(model_path))
-    model.to(config.device)
-    model.eval()
+    model = model.to(config.device)
+    model.eval()  # 将模型设置为评估模式
+    return model
 
-    # 加载和处理图像
-    dataset = FashionDataset(config.images_path, None)
-    image = dataset.load_image(image_path)
-    image = image.to(config.device)
+def process_image(image_path):
+    transform = transforms.Compose([
+        transforms.Resize((256, 256)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+    image = Image.open(image_path).convert('RGB')
+    image_tensor = transform(image).unsqueeze(0)  # 添加一个批次维度
+    return image_tensor
 
-    # 生成描述
-    with torch.no_grad():
-        caption = model.generate(image)
+def predict_caption(model, image_tensor, vocab, config):
+    # 生成束搜索描述
+    predictions = model.generate_by_beamsearch(image_tensor.to(config.device), config.beam_k, config.max_len)
+    # 将词索引转换回文字
+    idx_to_word = {idx: word for word, idx in vocab.items()}
+    caption_words = [idx_to_word.get(word, '<unk>') for word in predictions[0]]
+    caption = ' '.join(caption_words)
     return caption
+
+# 载入配置和词汇表
+config = Config()
+with open('../data/output/vocab.json', 'r') as f:
+    vocab = json.load(f)
+
+# 加载模型
+model_path = '../data/output/weights/.pth'  # 使用正确的模型文件路径
+model = load_model(model_path, vocab, config)
+
+# 处理图片并生成描述
+image_path = '../data/images/MEN-Denim-id_00000080-01_7_additional.jpg'  # 测试图片路径
+image_tensor = process_image(image_path)
+caption = predict_caption(model, image_tensor, vocab, config)
+
+print("Generated Caption:", caption)
